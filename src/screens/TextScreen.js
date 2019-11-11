8451105c9c470507aa5b73cccec367cbf72d95da
Elliot Burin <eburin2@gmail.com>
2019-11-10 18:00:57 -0800

    add TextScreen.js

---
+++ src/screens/TextScreen.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import { Text, StyleSheet, View, TextInput } from 'react-native';
+
+const TextScreen = () => {
+  const [name, setName] = useState('');
+  return(
+    <View>
+      <Text>Enter Name</Text>
+      <TextInput
+        autoCapitalize="none"
+        autoCorrect={false}
+        style={styles.input}
+        value={name}
+        onChangeText={(newValue) => setName(newValue)}
+      />
+      <Text>My name is: {name}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  input: {
+    margin: 15,
+    borderColor: 'black',
+    borderWidth: 1
+  }
+});
+
+export default TextScreen;
